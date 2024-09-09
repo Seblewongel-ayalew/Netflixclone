4b30b73ea20909c4b01352bb65a14bbb7355e439
Seblewongel-ayalew <seblilaw@gmail.com>
2024-09-10 01:17:51 +0300

    netflix clone

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Netflix-clone/",
+  base: "/Netflix-clone/", // This sets the base URL for your application, useful for GitHub Pages or subdirectory deployments
 });
